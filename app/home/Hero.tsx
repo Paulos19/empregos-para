27abcb0aead3bdfbf5fa90d3224919c6955ebe12
Paulos19 <paulohenrique.012araujo@gmail.com>
2024-10-23 16:30:44 -0300

    definitivo-47-nexx-3

--- app/home/Hero.tsx
+++ app/home/Hero.tsx
@@ -9,9 +9,9 @@ export const Hero = () => {
       <FlexboxSpacer maxWidth={75} minWidth={0} className="hidden lg:block" />
       <div className="mx-auto max-w-xl pt-8 text-center lg:mx-0 lg:grow lg:pt-32 lg:text-left">
         <h1 className="text-primary pb-1 text-4xl font-bold lg:text-5xl">
-          CRIE SEU CURRÍCULO
+          CRIE SEUS CURRÍCULOS
           <br />
-          PROFISSIONAL
+          PROFISSIONAIS
         </h1>
         <p className="mt-3 text-primary font-medium text-lg lg:mt-5 lg:text-xl">
           No conforto da sua casa
