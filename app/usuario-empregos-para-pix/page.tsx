6637b03f9aec422d9001ddca555ba96896495671
Paulos19 <paulohenrique.012araujo@gmail.com>
2024-10-23 17:05:31 -0300

    sem next

--- app/usuario-empregos-para-pix/page.tsx
+++ app/usuario-empregos-para-pix/page.tsx
@@ -40,38 +40,40 @@ const Usuarios = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-8 w-full max-w-4xl">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="bg-white shadow-lg rounded-lg p-4 w-full max-w-4xl">
         <h1 className="text-2xl font-bold text-primary mb-4 text-center">Lista de Usuários</h1>
         
-        <table className="table-auto w-full bg-gray-100 rounded-lg">
-          <thead>
-            <tr className="text-center bg-primary text-white">
-              <th className="px-4 py-2">Nome Completo</th>
-              <th className="px-4 py-2">Email</th>
-              <th className="px-4 py-2">Data e Hora</th>
-            </tr>
-          </thead>
-          <tbody>
-            {usuarios.length > 0 ? (
-              usuarios.map((usuario) => (
-                <tr key={usuario.id} className="text-center border-t">
-                  <td className="px-4 py-2">{usuario.fullName}</td>
-                  <td className="px-4 py-2">{usuario.email}</td>
-                  <td className="px-4 py-2">
-                    {new Date(usuario.createdAt).toLocaleString()} {/* Formata a data e hora */}
+        <div className="overflow-x-auto">
+          <table className="table-fixed w-full bg-gray-100 rounded-lg">
+            <thead>
+              <tr className="text-center bg-primary text-white">
+                <th className="px-4 py-2 w-1/3">Nome Completo</th>
+                <th className="px-4 py-2 w-1/3">Email</th>
+                <th className="px-4 py-2 w-1/3">Data e Hora</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usuarios.length > 0 ? (
+                usuarios.map((usuario) => (
+                  <tr key={usuario.id} className="text-center border-t">
+                    <td className="px-4 py-2">{usuario.fullName}</td>
+                    <td className="px-4 py-2">{usuario.email}</td>
+                    <td className="px-4 py-2">
+                      {new Date(usuario.createdAt).toLocaleString()} {/* Formata a data e hora */}
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={3} className="px-4 py-2 text-center">
+                    Nenhum usuário encontrado.
                   </td>
                 </tr>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={3} className="px-4 py-2 text-center">
-                  Nenhum usuário encontrado.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
