a355cfe72afe2585b3258799d63c917e6042bb13
Paulos19 <paulohenrique.012araujo@gmail.com>
2024-10-23 20:09:12 +0000

    seconcommit2

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"L3n6PC1sdo65fiaQIatu/VTggh7vTH2gAFAVwIPTGlY=\"}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jPfuzpJVB56PITnPLdPmim0GGwT0cuVkvjaDg3mVqbk=\"\n}"
->>>>>>> 55cc0da91f8fa4ea7d55cbc382ff8ee19ff41c22
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"/cfjtTF41Luys4LySBQAJlAAoZCF8mKnP94c+ODj1to=\"}"
